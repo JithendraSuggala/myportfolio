324041cfaf9d9877a2e769a61ed86785bc6cdfd5
Jithendra Suggala <jithendrasuggala77@gmail.com>
2024-08-13 19:22:51 +0530

    Updated Download Button with the Github Link

--- force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
+++ force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
@@ -5,6 +5,6 @@ export default class PortfolioUserDetails extends LightningElement {
     @api recordId
     @api objectApiName
     downloadResume(){
-        
+        window.open('https://github.com/JithendraSuggala/Resume/raw/main/JithendraResume.pdf','_blank');
     }
 }
