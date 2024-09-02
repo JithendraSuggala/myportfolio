41f8193c2d9f5c36ea0f3a6a539d73b53d3ad4a1
Jithendra Suggala <jithendrasuggala77@gmail.com>
2024-09-02 17:34:35 +0530

    Created Work Experience Tab

--- force-app/main/default/lwc/portfolioWorkExperience/portfolioWorkExperience.js
+++ force-app/main/default/lwc/portfolioWorkExperience/portfolioWorkExperience.js
@@ -1,9 +1,8 @@
 import { LightningElement,wire,api } from 'lwc';
 import { getRelatedListRecords } from 'lightning/uiRelatedListApi';
 export default class PortfolioWorkExperience extends LightningElement {
-
     @api recordId
-
+    workExperienceList = []
     @wire(getRelatedListRecords,{
         parentRecordId:'$recordId',
         relatedListId:'WorkExperience__r',
@@ -21,10 +20,31 @@ export default class PortfolioWorkExperience extends LightningElement {
     })workExperienceHandler({data,error}){
         if(data){
             console.log("Work Experience Data"+JSON.stringify(data));
+            this.formatContent(data);
         }
         if(error){
             console.error(error);
         }
     }
+    formatContent(data){
+      this.workExperienceList =  data.records.map(item=>{
+            let id=item.id
+            const {Role__c,WorkLocation__c,JobStartDate__c,
+                JobEndDate__c,CompanyName__c,StillWorkingHere__c,Description__c} =item.fields
+                let JobStartDate=this.getValue(JobStartDate__c)
+                let JobEndDate=this.getValue(JobEndDate__c)
+                let CompanyName=this.getValue(CompanyName__c)
+                let WorkLocation=this.getValue(WorkLocation__c)
+                let Role=this.getValue(Role__c)
+                let StillWorkingHere =this.getValue(StillWorkingHere__c)
+                let Description=this.getValue(Description__c)
+
+                return {id,Role,WorkLocation,JobStartDate,JobEndDate,CompanyName,StillWorkingHere,Description}
+        })
+        console.log("workExperienceList"+JSON.stringify(this.workExperienceList));
+    }
+    getValue(data){
+            return data && (data.displayValue || data.value);
+        }
 
 }
